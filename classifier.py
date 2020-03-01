f0e3d14ee61983a83494803dbbe1456129431a0d
Vineeth Bandi <38046649+vineeth-bandi@users.noreply.github.com>
2020-03-01 16:50:41 -0600

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,235 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import torchnet.meter.confusionmeter as cm
+
+# Data augmentation and normalization for training
+# Normalization for validation & test
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+}
+
+data_dir = r'C:\Users\Vineeth\Desktop\Vineeth\MISC\ML Projects\fruit_classifier\images'
+splits = ['train', 'val', 'test']
+image_datasets = {f: datasets.ImageFolder(os.path.join(data_dir, f),
+                                          data_transforms[f])
+                  for f in splits}
+dataloaders = {f: torch.utils.data.DataLoader(image_datasets[f], batch_size=32,
+                                             shuffle=True, num_workers=0)
+              for f in splits}
+dataset_sizes = {f: len(image_datasets[f]) for f in splits}
+class_names = image_datasets['train'].classes
+#print(class_names)
+#print(dataset_sizes)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#print(device)
+
+#lists for graph generation
+epoch_counter_train = []
+epoch_counter_val = []
+train_loss = []
+val_loss = []
+train_acc = []
+val_acc = []
+
+#Train the model
+def train_model(model, criterion, optimizer, scheduler, num_epochs):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch +1, num_epochs))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        tv_split = ['train', 'val']
+        for split in tv_split:
+            if split == 'train':
+                optimizer.step()
+                scheduler.step()
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[split]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(split == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if split == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            #For graph generation
+            if split == "train":
+                train_loss.append(running_loss/dataset_sizes[split])
+                train_acc.append(running_corrects.double() / dataset_sizes[split])
+                epoch_counter_train.append(epoch)
+            if split == "val":
+                val_loss.append(running_loss/ dataset_sizes[split])
+                val_acc.append(running_corrects.double() / dataset_sizes[split])
+                epoch_counter_val.append(epoch)
+
+            epoch_loss = running_loss / dataset_sizes[split]
+            epoch_acc = running_corrects.double() / dataset_sizes[split]
+
+            #for printing        
+            if split == "train":    
+                epoch_loss = running_loss / dataset_sizes[split]
+                epoch_acc = running_corrects.double() / dataset_sizes[split]
+            if split == "val":    
+                epoch_loss = running_loss / dataset_sizes[split]
+                epoch_acc = running_corrects.double() / dataset_sizes[split]
+            
+            
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                split, epoch_loss, epoch_acc))
+
+            # deep copy the best model
+            if split == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+#Using a model pre-trained on ImageNet and replacing it's final linear layer
+
+#For resnet50
+model_ft = models.resnet50(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, 15)
+
+model_ft = model_ft.to(device)
+
+criterion = nn.CrossEntropyLoss()
+
+# Using Adam as the parameter optimizer
+optimizer_ft = optim.Adam(model_ft.parameters(), lr = 0.001, betas=(0.9, 0.999))
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)       
+
+
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=25)        
+
+#Plot the train & validation losses
+plt.figure(1)
+plt.title("Training Vs Validation Losses")
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.plot(epoch_counter_train,train_loss,color = 'r', label="Training Loss")
+plt.plot(epoch_counter_val,val_loss,color = 'g', label="Validation Loss")
+plt.legend()
+plt.show()
+
+#Plot the accuracies in train & validation
+plt.figure(2)
+plt.title("Training Vs Validation Accuracies")
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.plot(epoch_counter_train,train_acc,color = 'r', label="Training Accuracy")
+plt.plot(epoch_counter_val,val_acc,color = 'g', label="Validation Accuracy")
+plt.legend()
+plt.show()
+
+#Test the accuracy with test data
+correct = 0
+total = 0
+with torch.no_grad():
+    for i, (inputs, labels) in enumerate(dataloaders['test']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model_ft(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the test images: %d %%' % (
+    100 * correct / total))
+
+#Class wise testing accuracy
+class_correct = list(0. for i in range(15))
+class_total = list(0. for i in range(15))
+with torch.no_grad():
+    for i, (inputs, labels) in enumerate(dataloaders['test']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model_ft(inputs)
+            _, predicted = torch.max(outputs, 1)
+            point = (predicted == labels).squeeze()
+            for j in range(len(labels)):
+                label = labels[j]
+                class_correct[label] += point[j].item()
+                class_total[label] += 1
+
+for i in range(15):
+    print('Accuracy of %5s : %2d %%' % (
+        class_names[i], 100 * class_correct[i] / class_total[i]))
+
+
+#Get the confusion matrix for testing data
+confusion_matrix = cm.ConfusionMeter(15)
+with torch.no_grad():
+    for i, (inputs, labels) in enumerate(dataloaders['test']):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+        outputs = model_ft(inputs)
+        _, predicted = torch.max(outputs, 1)
+        confusion_matrix.add(predicted, labels)
+    print(confusion_matrix.conf)
